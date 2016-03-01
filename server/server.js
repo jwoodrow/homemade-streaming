f30f9e976edb772c7432ff9f61450d38d92c3ca5
jwoodrow <jwoodrow@student.42.fr>
2016-03-01 14:50:19 +0100

    ok, torrents finish -> moved to public or ftp directory -> torrents removed -> db updated

--- server/server.js
+++ server/server.js
@@ -75,6 +75,7 @@ var createTorrent = function(source, callback){
       name: name,
       hash: hash,
       completion: "0%",
+      files: [],
       createdAt: new Date()
     });
     callback(source);
@@ -83,8 +84,16 @@ var createTorrent = function(source, callback){
   }
 };
 
-function cleanName(str) {
+var cleanName = function(str){
   return str.replace(/\.\./g,'').replace(/\//g,'');
+};
+
+var cleanPath = function(str){
+  return str.replace(/ /g, "\\ ").replace(/\]/g, "\\]").replace(/\[/g, "\\[");
+};
+
+var cleanerPath = function(str){
+  return str.replace(/ /g, "-").replace(/\]/g, "-").replace(/\[/g, "-");
 };
 
 var downloadFile = function(blob, name, encoding, callback){
@@ -106,12 +115,175 @@ var downloadFile = function(blob, name, encoding, callback){
 
   future.wait();
 
-  filepath = filepath.replace(/ /g, "\\ ");
-  filepath = filepath.replace(/\]/g, "\\]");
-  filepath = filepath.replace(/\[/g, "\\[");
+  filepath = cleanPath(filepath);
 
   callback(filepath, removeFile);
 };
+
+var removeTorrent = function(torrent){
+  var future = new Future();
+  cmd = "transmission-remote -n 'transmission:transmission' -t" + torrent.hash + " --remove-and-delete";
+  exec(cmd, function(error, stdout, stderr){
+    if (error){
+      console.log(error);
+    }
+    future.return();
+  });
+  future.wait();
+  Torrents.update(torrent._id, {
+    $set: {
+      hash: "COMPLETED"
+    }
+  });
+}
+
+var moveToFtp = function(torrent){
+  var hash = torrent.hash;
+  var basepath = torrent.location;
+  var files;
+  var cmd = "transmission-remote -n 'transmission:transmission' -t " + hash + " -f | awk 'NR > 2 {print substr($0,index($0,$7))}'";
+  var futureFiles = new Future();
+  exec(cmd, function(error, stdout, stderr){
+    if (error){
+      console.log(error);
+    }
+    files = stdout.toString().split("\n");
+    futureFiles.return();
+  });
+
+  futureFiles.wait();
+
+  futureRemoveTorrent = new Future();
+  var cmd = "transmission-remote -n 'transmission:transmission' -t " + hash + " -S";
+  exec(cmd, function(error, stdout, stderr){
+    if (error){
+      console.log(error);
+    }
+    futureRemoveTorrent.return();
+  });
+  futureRemoveTorrent.wait();
+
+  _.forEach(files, function(file, index){
+    if(file != ""){
+      originalpath = cleanPath(basepath + "/" + file);
+      missingDirs = file.split('/');
+      path = process.env.FTPDIR;
+      destpath = cleanerPath(path + "/" + file);
+      tmpPath = path;
+      if (missingDirs.length > 1){
+        _.forEach(missingDirs, function(dir, index){
+          if (index < missingDirs.length - 1){
+            tmpPath = cleanerPath(tmpPath + "/" + dir);
+            cmd = "mkdir -p " + tmpPath;
+            var futureDir = new Future();
+            exec(cmd, function(error, stdout, stderr){
+              if (error){
+                console.log(error);
+              }
+              futureDir.return();
+            });
+            futureDir.wait();
+          }
+        });
+      }
+      cmd = "mv " + originalpath + " " + destpath;
+      var futureCopy = new Future();
+      exec(cmd, function(error, stdout, stderr){
+        if (error){
+          console.log(error);
+        }
+        futureCopy.return();
+      });
+      futureCopy.wait();
+      tFiles = torrent.files;
+      tFiles[tFiles.length] = {
+        name: missingDirs[missingDirs.length],
+        path: "ftp://127.0.0.1/" + cleanerPath(file)
+      };
+      Torrents.update(torrent._id,
+      {
+        $set: {
+          files: tFiles,
+        }
+      });
+    }
+  });
+  removeTorrent(torrent);
+};
+
+var moveToPublic = function(torrent){
+  var hash = torrent.hash;
+  var basepath = torrent.location;
+  var files;
+  var cmd = "transmission-remote -n 'transmission:transmission' -t " + hash + " -f | awk 'NR > 2 {print substr($0,index($0,$7))}'";
+  var futureFiles = new Future();
+  exec(cmd, function(error, stdout, stderr){
+    if (error){
+      console.log(error);
+    }
+    files = stdout.toString().split("\n");
+    futureFiles.return();
+  });
+
+  futureFiles.wait();
+
+  futureRemoveTorrent = new Future();
+  var cmd = "transmission-remote -n 'transmission:transmission' -t " + hash + " -S";
+  exec(cmd, function(error, stdout, stderr){
+    if (error){
+      console.log(error);
+    }
+    futureRemoveTorrent.return();
+  });
+  futureRemoveTorrent.wait();
+
+  _.forEach(files, function(file, index){
+    if(file != ""){
+      originalpath = cleanPath(basepath + "/" + file);
+      missingDirs = file.split('/');
+      path = process.env.PWD + '/public';
+      destpath = cleanPath(path + "/" + file);
+      tmpPath = path;
+      if (missingDirs.length > 1){
+        _.forEach(missingDirs, function(dir, index){
+          if (index < missingDirs.length - 1){
+            tmpPath = cleanPath(tmpPath + "/" + dir);
+            cmd = "mkdir -p " + tmpPath;
+            var futureDir = new Future();
+            exec(cmd, function(error, stdout, stderr){
+              if (error){
+                console.log(error);
+              }
+              futureDir.return();
+            });
+            futureDir.wait();
+          }
+        });
+      }
+      cmd = "mv " + originalpath + " " + destpath;
+      var futureCopy = new Future();
+      exec(cmd, function(error, stdout, stderr){
+        if (error){
+          console.log(error);
+        }
+        futureCopy.return();
+      });
+      futureCopy.wait();
+      tFiles = torrent.files;
+      tFiles[tFiles.length] = {
+        name: missingDirs[missingDirs.length],
+        path: file
+      };
+      Torrents.update(torrent._id,
+      {
+        $set: {
+          files: tFiles,
+        }
+      });
+    }
+  });
+  removeTorrent(torrent);
+}
 
 Meteor.methods({
   addTorrent: function(magnet){
@@ -149,17 +321,20 @@ Meteor.methods({
       });
       future.wait();
       name = res[2];
+      location = res[8].trim();
       percentage = res[9].trim();
       if (name != torrent.name || torrent.completion != "100%"){
         Torrents.update(torrentId, {
           $set: {
             name: name,
+            location: location,
             completion: percentage
           }
         });
       }
       if (percentage == "100%"){
         console.log("move files to ftp");
+        moveToPublic(torrent);
       }
     }
   }
@@ -173,7 +348,7 @@ SyncedCron.add({
   },
   job: function() {
     torrents = Torrents.find().fetch();
-    _.each(torrents, function(torrent, index){
+    _.forEach(torrents, function(torrent, index){
       Meteor.call("updateTorrent", torrent._id, function(){
 
       });
